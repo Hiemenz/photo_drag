485d92a9a9a4cade4fc60909fe879526eca89e90
kah5161 <kah5161@rit.edu>
2020-05-06 15:30:14 -0400

    added the photo select all

--- photo_drag.py
+++ photo_drag.py
@@ -12,8 +12,22 @@ right_side = (866, 401)
 
 print(pyautogui.size())
 
+print('Clicking the first tab Google Photos')
 pyautogui.click(first_tab)
 time.sleep(1)
+print('Reloading the window')
+pyautogui.hotkey("ctrlleft", "r")
+time.sleep(4)
+
+print('Clicking the second tab Photos Folder')
+pyautogui.click(second_tab)
+time.sleep(1)
+print('Selecting all the photos')
+pyautogui.click(right_side)
+
+pyautogui.hotkey("ctrlleft", "a")
+
+
 print(pyautogui.position())
 
 
