62c2be24b51976b4a2a05dc62d994481fd54d02a
kah5161 <kah5161@rit.edu>
2020-05-06 15:20:38 -0400

    added a click

--- photo_drag.py
+++ photo_drag.py
@@ -1,6 +1,19 @@
 import pyautogui
+import time
+
+# google tab
+first_tab = (158, 10)
+
+left_side = (187, 583)
+
+# photo folder tab
+second_tab = (327, 6)
+right_side = (866, 401)
 
 print(pyautogui.size())
 
-
+pyautogui.click(first_tab)
+time.sleep(1)
 print(pyautogui.position())
+
+
