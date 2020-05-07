a3087549ea5cc2d65c09e46de0045625cf5bbc24
kah5161 <kah5161@rit.edu>
2020-05-07 00:12:08 -0400

    added scheduling

--- photo_drag.py
+++ photo_drag.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-# 
-
-
+import schedule
 import pyautogui
 import time
 
@@ -16,25 +14,30 @@ right_side_two = (866, 131)
 print(pyautogui.position())
 print(pyautogui.size())
 
-print('Clicking the first tab Google Photos')
-pyautogui.click(first_tab)
-time.sleep(1)
-print('Reloading the window')
-pyautogui.hotkey("ctrlleft", "r")
-time.sleep(4)
+def job():
+    print('Clicking the first tab Google Photos')
+    pyautogui.click(first_tab)
+    time.sleep(1)
+    print('Reloading the window')
+    pyautogui.hotkey("ctrlleft", "r")
+    time.sleep(4)
 
-print('Clicking the second tab Photos Folder')
-pyautogui.click(second_tab)
-time.sleep(1)
-print('Selecting all the photos')
-pyautogui.click(right_side_one)
+    print('Clicking the second tab Photos Folder')
+    pyautogui.click(second_tab)
+    time.sleep(1)
+    print('Selecting all the photos')
+    pyautogui.click(right_side_one)
 
-pyautogui.hotkey("ctrlleft", "a")
-time.sleep(5)
-pyautogui.moveTo(right_side_two) 
+    pyautogui.hotkey("ctrlleft", "a")
+    time.sleep(5)
+    pyautogui.moveTo(right_side_two) 
 
-pyautogui.dragTo(left_side, duration = 2)
+    pyautogui.dragTo(left_side, duration = 2)
+
+# schedule.every().day.at("19:30").do(job)
+schedule.every().day.at("00:13").do(job)
 
 
-
-
+while True:
+    schedule.run_pending()
+    time.sleep(20)
